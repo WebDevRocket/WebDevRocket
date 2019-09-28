eb285f65c2fc5ea56e688295b23c4b136b877313
Wojciech Zawistowski <wojciech.zawistowski@gmail.com>
2019-09-28 20:49:23 +0200

    Remove Bio component and stub twitter handle

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,9 +8,6 @@ module.exports = {
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl,
     disqusShortname: `webdevrocket`,
-    social: {
-      twitter: `kylemathews`,
-    },
   },
   plugins: [
     {
