1e3d127b3136151a70842cf2a92155aa4689d98a
Wojciech Zawistowski <wojciech.zawistowski@gmail.com>
2020-05-27 17:50:03 +0200

    LAyout cleanup

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,16 +4,6 @@ import styled from "styled-components"
 import Header from "./header"
 import Footer from "./footer"
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100%;
-`
-
-const MainWrapper = styled.div`
-  flex-grow: 1;
-`
-
 const Main = styled.main`
   max-width: var(--main-width);
   margin: 0 auto;
@@ -25,13 +15,11 @@ const Main = styled.main`
 
 const Layout = ({ children }) => {
   return (
-    <Wrapper>
+    <>
       <Header />
-      <MainWrapper>
-        <Main>{children}</Main>
-      </MainWrapper>
+      <Main>{children}</Main>
       <Footer />
-    </Wrapper>
+    </>
   )
 }
 
